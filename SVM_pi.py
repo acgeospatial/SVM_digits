c79caf64bc819d49ca5c893bf6835496864c67c5
Andrew Cutts <acutts@acgeospatial.co.uk>
2021-12-11 11:14:30 +0000

    removed time calls

--- SVM_pi.py
+++ SVM_pi.py
@@ -13,11 +13,7 @@ import matplotlib.pyplot as plt
 from sklearn import datasets
 from sklearn import svm
 
-### Delete following line if not interested in time to run code
-print ("time to import libraries: ", time.time()) - time_start
-print ("------")
-### Delete following line if not interested in time to run code
-time_start2 = time.time()
+
 ### Number of training samples to not train on, but test
 not_train_no = 10
 ##' load the data
@@ -31,18 +27,16 @@ X,y = digits.data[:-not_train_no], digits.target[:-not_train_no]
 clf.fit(X,y)
 
 for i in range(1,(not_train_no +1)):
-	### Loop through the not trained data and make a prediction using SVM
-	print "SVM predicts this number: ", clf.predict(digits.data[[-i]])
-	### Strings for the figure header
-	Pred = str(clf.predict(digits.data[[-i]]))
-	Actual = str(digits.target[-i])
-	figureHead = "Prediction: " + Pred + " Labelled as: " + Actual
-	### Delete following line if not interested in time to run code
-	print "time to train data set, run SVM and make prediction: ", time.time() - time_start2
-	### create the plot and set the figure title to prediction and labelled
-	fig = plt.figure(1)
-	fig.canvas.set_window_title(figureHead)
-	plt.imshow(digits.images[-i], cmap=plt.cm.gray_r, interpolation='nearest')
-	plt.show()
-	### save figure?
-	### savefig('/.png')
+    ### Loop through the not trained data and make a prediction using SVM
+    print("SVM predicts this number: ", clf.predict(digits.data[[-i]]))
+    ### Strings for the figure header
+    Pred = str(clf.predict(digits.data[[-i]]))
+    Actual = str(digits.target[-i])
+    figureHead = "Prediction: " + Pred + " Labelled as: " + Actual
+    ### create the plot and set the figure title to prediction and labelled
+    fig = plt.figure(1)
+    fig.canvas.set_window_title(figureHead)
+    plt.imshow(digits.images[-i], cmap=plt.cm.gray_r, interpolation='nearest')
+    plt.show()
+    ### save figure?
+    ### savefig('/.png')
